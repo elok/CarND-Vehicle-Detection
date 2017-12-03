e15995be835cc0987e8cdd104306782337051e5c
ericlok <ericlok@gmail.com>
2017-12-03 18:43:34 -0500

    starting a better structure

---
+++ util.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    """
+    Define a function that takes an image, a list of bounding boxes,
+    and optional color tuple and line thickness as inputs
+    then draws boxes in that color on the output
+    :param img:
+    :param bboxes:
+    :param color:
+    :param thick:
+    :return:
+    """
+    # Make a copy of the image
+    draw_img = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return draw_img
+
+# Define a function to compute color histogram features
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the RGB channels separately
+    rhist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    ghist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    bhist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Generating bin centers
+    bin_edges = rhist[1]
+    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return rhist, ghist, bhist, bin_centers, hist_features
+
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+# KEEP IN MIND IF YOU DECIDE TO USE THIS FUNCTION LATER
+# IN YOUR PROJECT THAT IF YOU READ THE IMAGE WITH
+# cv2.imread() INSTEAD YOU START WITH BGR COLOR!
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(feature_image, size).ravel()
+    # Return the feature vectoratures)))
+    # Return list of feature vectors
+    return features
