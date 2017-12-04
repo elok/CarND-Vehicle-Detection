57d8446b72a806912ff2a328855aad049e165e6e
Eric Lok <ericlok@gmail.com>
2017-12-03 23:37:19 -0500

    testing out all the util functions

--- scratchpad.py
+++ scratchpad.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2
+import glob
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from util import color_hist
+from sklearn.preprocessing import StandardScaler
+from util import color_hist, plot3d, bin_spatial, data_look, get_hog_features, extract_features
 
 def test_histogram():
-    image = mpimg.imread('cutout1.jpg')
+    image = cv2.imread(r'vehicles/GTI_Far/image0001.png')
 
     rh, gh, bh, bincen, feature_vec = color_hist(image, nbins=32, bins_range=(0, 256))
 
@@ -25,8 +27,155 @@ def test_histogram():
         plt.xlim(0, 256)
         plt.title('B Histogram')
         fig.tight_layout()
+        plt.show()
     else:
         print('Your function is returning None for at least one variable...')
 
+def test_bin_spatial():
+    image = cv2.imread(r'vehicles/GTI_Far/image0001.png')
+
+    feature_vec = bin_spatial(image, color_space='RGB', size=(32, 32))
+
+    # Plot features
+    plt.plot(feature_vec)
+    plt.title('Spatially Binned Features')
+    plt.show()
+
+def test_plot3d():
+    # Read a color image
+    img = cv2.imread(r'scratchpad_images/000275.png')
+    # img = cv2.imread(r'scratchpad_images/000528.png')
+    # img = cv2.imread(r'scratchpad_images/001240.png')
+
+    # Select a small fraction of pixels to plot by subsampling it
+    scale = max(img.shape[0], img.shape[1], 64) / 64  # at most 64 rows and columns
+    img_small = cv2.resize(img, (np.int(img.shape[1] / scale), np.int(img.shape[0] / scale)),
+                           interpolation=cv2.INTER_NEAREST)
+
+    # Convert subsampled image to desired color space(s)
+    img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes RGB
+    img_small_HSV = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
+    img_small_rgb = img_small_RGB / 255.  # scaled to [0, 1], only for plotting
+
+    # Plot and show
+    plot3d(img_small_RGB, img_small_rgb)
+    plt.show()
+
+    plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
+    plt.show()
+
+def test_datalook():
+    cars = glob.glob(r'vehicles/GTI_Far/*.png')
+    notcars = glob.glob(r'non-vehicles/GTI/*.png')
+
+    # for image in images:
+        # if 'image' in image or 'extra' in image:
+        #     notcars.append(image)
+        # else:
+        #     cars.append(image)
+
+    data_info = data_look(cars, notcars)
+
+    print('Your function returned a count of', data_info["n_cars"], ' cars and', data_info["n_notcars"], ' non-cars')
+    print('of size: ', data_info["image_shape"], ' and data type:', data_info["data_type"])
+    # Just for fun choose random car / not-car indices and plot example images
+    car_ind = np.random.randint(0, len(cars))
+    notcar_ind = np.random.randint(0, len(notcars))
+
+    # Read in car / not-car images
+    car_image = mpimg.imread(cars[car_ind])
+    notcar_image = mpimg.imread(notcars[notcar_ind])
+
+    # Plot the examples
+    fig = plt.figure()
+    plt.subplot(121)
+    plt.imshow(car_image)
+    plt.title('Example Car Image')
+    plt.subplot(122)
+    plt.imshow(notcar_image)
+    plt.title('Example Not-car Image')
+    plt.show()
+
+def test_hog():
+    # Read in our vehicles and non-vehicles
+    cars = glob.glob(r'vehicles/GTI_Far/*.png')
+    notcars = glob.glob(r'non-vehicles/GTI/*.png')
+
+    # Generate a random index to look at a car image
+    ind = np.random.randint(0, len(cars))
+    # Read in the image
+    image = mpimg.imread(cars[ind])
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # Define HOG parameters
+    orient = 9
+    pix_per_cell = 8
+    cell_per_block = 2
+    # Call our function with vis=True to see an image output
+    features, hog_image = get_hog_features(gray, orient,
+                                           pix_per_cell, cell_per_block,
+                                           vis=True, feature_vec=False)
+
+    # Plot the examples
+    fig = plt.figure()
+    plt.subplot(121)
+    plt.imshow(image, cmap='gray')
+    plt.title('Example Car Image')
+    plt.subplot(122)
+    plt.imshow(hog_image, cmap='gray')
+    plt.title('HOG Visualization')
+    plt.show()
+
+def test_extract_features():
+    # Read in our vehicles and non-vehicles
+    cars = []
+    notcars = []
+
+    # cars
+    images = glob.glob('vehicles/**/*.png', recursive=True)  # cars
+    for image in images:
+        cars.append(image)
+        break
+
+    # non-cars
+    images = glob.glob('non-vehicles/**/*.png', recursive=True)  # noncars
+    for image in images:
+        notcars.append(image)
+        break
+
+    no_of_images = [len(cars), len(notcars)]
+    print(no_of_images)
+
+    car_features = extract_features(cars, cspace='RGB', spatial_size=(32, 32), hist_bins=32, hist_range=(0, 256))
+    notcar_features = extract_features(notcars, cspace='RGB', spatial_size=(32, 32), hist_bins=32, hist_range=(0, 256))
+
+    if len(car_features) > 0:
+        # Create an array stack of feature vectors
+        X = np.vstack((car_features, notcar_features)).astype(np.float64)
+        # Fit a per-column scaler
+        X_scaler = StandardScaler().fit(X)
+        # Apply the scaler to X
+        scaled_X = X_scaler.transform(X)
+        car_ind = np.random.randint(0, len(cars))
+        # Plot an example of raw and scaled features
+        fig = plt.figure(figsize=(12, 4))
+        plt.subplot(131)
+        plt.imshow(mpimg.imread(cars[car_ind]))
+        plt.title('Original Image')
+        plt.subplot(132)
+        plt.plot(X[car_ind])
+        plt.title('Raw Features')
+        plt.subplot(133)
+        plt.plot(scaled_X[car_ind])
+        plt.title('Normalized Features')
+        fig.tight_layout()
+        plt.show()
+    else:
+        print('Your function only returns empty feature vectors...')
+
 if __name__ == '__main__':
-    pass
+    # test_histogram()
+    # test_bin_spatial()
+    # test_plot3d()
+    # test_datalook()
+    # test_hog()
+    test_extract_features()
